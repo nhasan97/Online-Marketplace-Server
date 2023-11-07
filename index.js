538211b38636c85e2e3f285d1a9aa904994993f8
nhasan97 <nh6069325@gmail.com>
2023-11-08 00:14:36 +0600

    created api for fetching logged in user's bids

--- index.js
+++ index.js
@@ -72,6 +72,21 @@ async function run() {
       }
     });
 
+    app.get("/bids", async (req, res) => {
+      try {
+        const bidsCollection = database.collection("bids");
+        let query = {};
+        if (req.query?.email) {
+          query = { bidderEmail: req.query.email };
+        }
+        const cursor = bidsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        res.send({ error: true, message: error.message });
+      }
+    });
+
     //============================== posts ==============================
 
     app.post("/posted-jobs", async (req, res) => {
