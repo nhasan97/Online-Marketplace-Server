65af85432e67d2ec4cd4d4b034188689d7f45ed5
nhasan97 <nh6069325@gmail.com>
2023-11-09 19:25:01 +0600

    created api for getiing data of reviews and popularity graph

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = 5000 || process.env.PORT;
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
+      "http://localhost:5173",
       "https://assignment-eleven-487e9.web.app",
       "https://assignment-eleven-487e9.firebaseapp.com",
     ],
@@ -59,6 +59,17 @@ async function run() {
     const database = client.db("marketplaceDB");
 
     //============================== gets ==============================
+
+    app.get("/categories", async (req, res) => {
+      try {
+        const categoriesCollection = database.collection("categories");
+        const cursor = categoriesCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        res.send({ error: true, message: error.message });
+      }
+    });
 
     app.get("/posted-jobs", async (req, res) => {
       try {
@@ -132,6 +143,36 @@ async function run() {
         const cursor = bidsCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
+      } catch (error) {
+        res.send({ error: true, message: error.message });
+      }
+    });
+
+    app.get("/reviews", async (req, res) => {
+      try {
+        const portfolioCollection = database.collection("portfolio");
+        const cursor = portfolioCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        res.send({ error: true, message: error.message });
+      }
+    });
+
+    app.get("/popularity", async (req, res) => {
+      try {
+        const servicesCollection = database.collection("postedJobs");
+        const query1 = { category: "Web Development" };
+        const result1 = await servicesCollection.countDocuments(query1);
+        const query2 = { category: "Digital Marketing" };
+        const result2 = await servicesCollection.countDocuments(query2);
+        const query3 = { category: "Graphic Design" };
+        const result3 = await servicesCollection.countDocuments(query3);
+        res.send([
+          { name: "Web Development", count: result1 },
+          { name: "Digital Marketing", count: result2 },
+          { name: "Graphic Design", count: result3 },
+        ]);
       } catch (error) {
         res.send({ error: true, message: error.message });
       }
